228ff24bcd2565fc66b67c3d86fccf5efc488953
Joe Shymanski <jshyman517@gmail.com>
2022-02-15 23:53:41 -0600

    Added random forest part, min examples and max depth stopping criteria

--- random_forest.py
+++ random_forest.py
@@ -90,6 +90,11 @@ def importance(f, examples, features, measure):
     if isinstance(features[f], tuple):
         # Handle numeric data
         exs = sorted(examples, key=lambda e: e[f])
+        # TODO: Take random subset of range
+        # L = exs[0][f]
+        # H = exs[-1][f]
+        # line = random.uniform(L, H)
+        # greater = random.getrandbits(1)
         mids = []
         for i in range(len(exs) - 1):
             e1 = exs[i]
@@ -123,35 +128,43 @@ def argmax(features, examples, impurity_measure):
             F, V, split = f, v, s
     return F, split
 
-def learn_decision_tree(examples, features, parent_examples, impurity_measure, min_examples=400):
+def learn_decision_tree(examples, features, parent_examples, impurity_measure, min_examples=1, max_depth=float("inf"), depth=0):
     if not examples:
         return Decision_Tree(plurality_value(parent_examples))
     elif all_equal(examples):
         return Decision_Tree(examples[0]["class"])
-    elif not features or len(examples) < min_examples:
+    elif not features or len(examples) < min_examples or depth == max_depth:
         return Decision_Tree(plurality_value(examples))
     else:
+        # Random subset of features at each node
         keys = random.sample(list(features), math.floor(math.log2(len(features) + 1)))
         feature_subset = {k: features[k] for k in keys}
+
         F, split = argmax(feature_subset, examples, impurity_measure)
         tree = Decision_Tree(F)
         if split:
             # Handle numeric data
             split = round(split, 9)
             low_exs = [e for e in examples if e[F] <= split]
-            low_subtree = learn_decision_tree(low_exs, features, examples, impurity_measure, min_examples)
+            low_subtree = learn_decision_tree(low_exs, features, examples, impurity_measure, min_examples, max_depth, depth + 1)
             tree.add_branch({"branch_label": "<=" + str(split), "node": low_subtree})
 
             high_exs = [e for e in examples if e[F] > split]
-            high_subtree = learn_decision_tree(high_exs, features, examples, impurity_measure, min_examples)
+            high_subtree = learn_decision_tree(high_exs, features, examples, impurity_measure, min_examples, max_depth, depth + 1)
             tree.add_branch({"branch_label": ">" + str(split), "node": high_subtree})
         else:
             for v in features[F]:
                 feats = {k: val for k, val in features.items() if k != F}
                 exs = [e for e in examples if e[F] == v]
-                subtree = learn_decision_tree(exs, feats, examples, impurity_measure, min_examples)
+                subtree = learn_decision_tree(exs, feats, examples, impurity_measure, min_examples, max_depth, depth + 1)
                 tree.add_branch({"branch_label": v, "node": subtree})
         return tree
+
+def random_forest(examples, features, parent_examples, impurity_measure, num_trees=50, min_examples=1, max_depth=float("Inf")):
+    forest = []
+    for _ in tqdm(range(num_trees), desc="Generating Forest"):
+        forest.append(learn_decision_tree(examples, features, parent_examples, impurity_measure, min_examples, max_depth))
+    return forest
 
 def n_folds(N, examples):
     random.shuffle(examples)
@@ -167,200 +180,29 @@ def n_folds(N, examples):
         folds.append({"train": train_exs, "test": test_exs})
     return folds
 
+def accuracy(forest, examples):
+    accuracy = 0
+    for ex in tqdm(examples, desc="Calculating Accuracy"):
+        votes = {}
+        for tree in forest:
+            prediction = tree.decide(ex)
+            if prediction not in votes:
+                votes[prediction] = 0
+            votes[prediction] += 1
+        if ex["class"] == max(votes, key=votes.get):
+            accuracy += 1
+    return accuracy/len(examples)*100
+
 if __name__ == "__main__":
-    restaurant_features = {
-        "Alt":      ["Yes", "No"],
-        "Bar":      ["Yes", "No"],
-        "Fri":      ["Yes", "No"],
-        "Hun":      ["Yes", "No"],
-        "Pat":      ["None", "Some", "Full"],
-        "Price":    ["$", "$$", "$$$"],
-        "Rain":     ["Yes", "No"],
-        "Res":      ["Yes", "No"],
-        "Type":     ["French", "Thai", "Burger", "Italian"],
-        "Est":      ["0-10", "10-30", "30-60", ">60"]
-    }
-    restaurant_examples = [
-        {"Alt": "Yes",  "Bar": "No",    "Fri": "No",    "Hun": "Yes",   "Pat": "Some", "Price": "$$$",  "Rain": "No",   "Res": "Yes",   "Type": "French",   "Est": "0-10",     "class": "Yes"},
-        {"Alt": "Yes",  "Bar": "No",    "Fri": "No",    "Hun": "Yes",   "Pat": "Full", "Price": "$",    "Rain": "No",   "Res": "No",    "Type": "Thai",     "Est": "30-60",    "class": "No"},
-        {"Alt": "No",   "Bar": "Yes",   "Fri": "No",    "Hun": "No",    "Pat": "Some", "Price": "$",    "Rain": "No",   "Res": "No",    "Type": "Burger",   "Est": "0-10",     "class": "Yes"},
-        {"Alt": "Yes",  "Bar": "No",    "Fri": "Yes",   "Hun": "Yes",   "Pat": "Full", "Price": "$",    "Rain": "Yes",  "Res": "Yes",   "Type": "Thai",     "Est": "10-30",    "class": "Yes"},
-        {"Alt": "Yes",  "Bar": "No",    "Fri": "Yes",   "Hun": "No",    "Pat": "Full", "Price": "$$$",  "Rain": "No",   "Res": "Yes",   "Type": "French",   "Est": ">60",      "class": "No"},
-        {"Alt": "No",   "Bar": "Yes",   "Fri": "No",    "Hun": "Yes",   "Pat": "Some", "Price": "$$",   "Rain": "Yes",  "Res": "Yes",   "Type": "Italian",  "Est": "0-10",     "class": "Yes"},
-        {"Alt": "No",   "Bar": "Yes",   "Fri": "No",    "Hun": "No",    "Pat": "None", "Price": "$",    "Rain": "Yes",  "Res": "No",    "Type": "Burger",   "Est": "0-10",     "class": "No"},
-        {"Alt": "No",   "Bar": "No",    "Fri": "No",    "Hun": "Yes",   "Pat": "Some", "Price": "$$",   "Rain": "Yes",  "Res": "Yes",   "Type": "Thai",     "Est": "0-10",     "class": "Yes"},
-        {"Alt": "No",   "Bar": "Yes",   "Fri": "Yes",   "Hun": "No",    "Pat": "Full", "Price": "$",    "Rain": "Yes",  "Res": "No",    "Type": "Burger",   "Est": ">60",      "class": "No"},
-        {"Alt": "Yes",  "Bar": "Yes",   "Fri": "Yes",   "Hun": "Yes",   "Pat": "Full", "Price": "$$$",  "Rain": "No",   "Res": "Yes",   "Type": "Italian",  "Est": "10-30",    "class": "No"},
-        {"Alt": "No",   "Bar": "No",    "Fri": "No",    "Hun": "No",    "Pat": "None", "Price": "$",    "Rain": "No",   "Res": "No",    "Type": "Thai",     "Est": "0-10",     "class": "No"},
-        {"Alt": "Yes",  "Bar": "Yes",   "Fri": "Yes",   "Hun": "Yes",   "Pat": "Full", "Price": "$",    "Rain": "No",   "Res": "No",    "Type": "Burger",   "Est": "30-60",    "class": "Yes"}
-    ]
-
-    proj1_features = {
-        "x": (0, 1),
-        "y": (0, 1)
-    }
-    proj1_examples = []
-    train_file = open("train-file", "r")
-    lines = train_file.readlines()
-    for line in lines:
-        line = line[:-1]
-        values = line.replace("\"", "").replace(",", " ").split(" ")
-        proj1_examples.append({
-            "x":        float(values[1]),
-            "y":        float(values[2]),
-            "class":    int(values[0])
-        })
-
-    zoo_features = {
-        "hair":     [True, False],
-        "feathers": [True, False],
-        "eggs":     [True, False],
-        "milk":     [True, False],
-        "airborne": [True, False],
-        "aquatic":  [True, False],
-        "predator": [True, False],
-        "toothed":  [True, False],
-        "backbone": [True, False],
-        "breathes": [True, False],
-        "venemous": [True, False],
-        "fins":     [True, False],
-        "legs":     [0, 2, 4, 5, 6, 8],
-        "tail":     [True, False],
-        "domestic": [True, False],
-        "catsize":  [True, False]
-    }
-    zoo_examples = []
-    train_file = open("zoo.data", "r")
-    lines = train_file.readlines()
-    classes = ["mammal", "bird", "reptile", "fish", "amphibian", "insect", "invertebrate"]
-    for line in lines:
-        line = line[:-1]
-        values = line.replace("\"", "").replace(",", " ").split(" ")
-        zoo_examples.append({
-            "hair":     bool(int(values[1])),
-            "feathers": bool(int(values[2])),
-            "eggs":     bool(int(values[3])),
-            "milk":     bool(int(values[4])),
-            "airborne": bool(int(values[5])),
-            "aquatic":  bool(int(values[6])),
-            "predator": bool(int(values[7])),
-            "toothed":  bool(int(values[8])),
-            "backbone": bool(int(values[9])),
-            "breathes": bool(int(values[10])),
-            "venemous": bool(int(values[11])),
-            "fins":     bool(int(values[12])),
-            "legs":     int(values[13]),
-            "tail":     bool(int(values[14])),
-            "domestic": bool(int(values[15])),
-            "catsize":  bool(int(values[16])),
-            "class":    classes[int(values[17]) - 1]
-        })
-
-    letter_features = {
-        "x-box horizontal position of box":     (0, 15),
-        "y-box vertical position of box":       (0, 15),
-        "width width of box":                   (0, 15),
-        "high height of box":                   (0, 15),
-        "onpix total # on pixels":              (0, 15),
-        "x-bar mean x of on pixels in box":     (0, 15),
-        "y-bar mean y of on pixels in box":     (0, 15),
-        "x2bar mean x variance":                (0, 15),
-        "y2bar mean y variance":                (0, 15),
-        "xybar mean x y correlation":           (0, 15),
-        "x2ybr mean of x * x * y":              (0, 15),
-        "xy2br mean of x * y * y":              (0, 15),
-        "x-ege mean edge count left to right":  (0, 15),
-        "xegvy correlation of x-ege with y":    (0, 15),
-        "y-ege mean edge count bottom to top":  (0, 15),
-        "yegvx correlation of y-ege with x":    (0, 15)
-    }
-    letter_examples = []
-    train_file = open("letter-recognition.data", "r")
-    lines = train_file.readlines()
-    for line in lines:
-        line = line[:-1]
-        values = line.replace("\"", "").replace(",", " ").split(" ")
-        letter_examples.append({
-            "x-box horizontal position of box":     int(values[1]),
-            "y-box vertical position of box":       int(values[2]),
-            "width width of box":                   int(values[3]),
-            "high height of box":                   int(values[4]),
-            "onpix total # on pixels":              int(values[5]),
-            "x-bar mean x of on pixels in box":     int(values[6]),
-            "y-bar mean y of on pixels in box":     int(values[7]),
-            "x2bar mean x variance":                int(values[8]),
-            "y2bar mean y variance":                int(values[9]),
-            "xybar mean x y correlation":           int(values[10]),
-            "x2ybr mean of x * x * y":              int(values[11]),
-            "xy2br mean of x * y * y":              int(values[12]),
-            "x-ege mean edge count left to right":  int(values[13]),
-            "xegvy correlation of x-ege with y":    int(values[14]),
-            "y-ege mean edge count bottom to top":  int(values[15]),
-            "yegvx correlation of y-ege with x":    int(values[16]),
-            "class":                                str(values[0])
-        })
-
-    # print("Restaurant")
-    # N = 5
-    # max_depth = 5
-    # restaurant_folds = n_folds(N, restaurant_examples)
-    # restaurant_train_accuracies = restaurant_test_accuracies = 0
-    # for fold in tqdm(restaurant_folds):
-    #     restaurant_tree = learn_decision_tree(fold["train"], restaurant_features, [], entropy, 5)
-    #     restaurant_train_accuracies += restaurant_tree.accuracy(fold["train"])
-    #     restaurant_test_accuracies += restaurant_tree.accuracy(fold["test"])
-    # print("Average training set accuracy: " + str(round(restaurant_train_accuracies / N, 2)) + "%")
-    # print("Average testing set accuracy: " + str(round(restaurant_test_accuracies / N, 2)) + "%")
-    # print()
-
-    # print("Proj1")
-    # N = 5
-    # max_depth = 5
-    # proj1_folds = n_folds(N, proj1_examples)
-    # proj1_train_accuracies = proj1_test_accuracies = 0
-    # for fold in tqdm(proj1_folds):
-    #     proj1_tree = learn_decision_tree(fold["train"], proj1_features, [], entropy, max_depth)
-    #     proj1_train_accuracies += proj1_tree.accuracy(fold["train"])
-    #     proj1_test_accuracies += proj1_tree.accuracy(fold["test"])
-    # print("Average training set accuracy: " + str(round(proj1_train_accuracies / N, 2)) + "%")
-    # print("Average testing set accuracy: " + str(round(proj1_test_accuracies / N, 2)) + "%")
-    # print()
-
     N = 5
-    num_trees = 50
-    proj1_folds = n_folds(N, proj1_examples)
-    proj1_train_accuracies = proj1_test_accuracies = 0
-    proj1_tree = learn_decision_tree(proj1_folds[0]["train"], proj1_features, [], entropy)
-    print(proj1_tree)
-    proj1_train_accuracies += proj1_tree.accuracy(proj1_folds[0]["train"])
-    proj1_test_accuracies += proj1_tree.accuracy(proj1_folds[0]["test"])
-    print("Average training set accuracy: " + str(round(proj1_train_accuracies, 2)) + "%")
-    print("Average testing set accuracy: " + str(round(proj1_test_accuracies, 2)) + "%")
+    examples = {}
+    features = {}
+    folds = n_folds(N, examples)
+    train_accuracies = test_accuracies = 0
+    for fold in folds:
+        forest = random_forest(fold["train"], features, [], entropy, num_trees=50)
+        train_accuracies += accuracy(forest, fold["train"])
+        test_accuracies += accuracy(forest, fold["test"])
+    print("Average training set accuracy: " + str(round(train_accuracies / N, 2)) + "%")
+    print("Average testing set accuracy: " + str(round(test_accuracies / N, 2)) + "%")
     print()
-
-    # print("Zoo")
-    # N = 5
-    # max_depth = 5
-    # zoo_folds = n_folds(N, zoo_examples)
-    # zoo_train_accuracies = zoo_test_accuracies = 0
-    # for fold in tqdm(zoo_folds):
-    #     zoo_tree = learn_decision_tree(fold["train"], zoo_features, [], entropy, max_depth)
-    #     zoo_train_accuracies += zoo_tree.accuracy(fold["train"])
-    #     zoo_test_accuracies += zoo_tree.accuracy(fold["test"])
-    # print("Average training set accuracy: " + str(round(zoo_train_accuracies / N, 2)) + "%")
-    # print("Average testing set accuracy: " + str(round(zoo_test_accuracies / N, 2)) + "%")
-    # print()
-
-    # print("Letters")
-    # N = 5
-    # max_depth = 5
-    # letter_sample = random.sample(letter_examples, 500)
-    # letter_folds = n_folds(N, letter_sample)
-    # letter_train_accuracies = letter_test_accuracies = 0
-    # for fold in tqdm(letter_folds):
-    #     letter_tree = learn_decision_tree(fold["train"], letter_features, [], entropy, max_depth)
-    #     letter_train_accuracies += letter_tree.accuracy(fold["train"])
-    #     letter_test_accuracies += letter_tree.accuracy(fold["test"])
-    # print("Average training set accuracy: " + str(round(letter_train_accuracies / N, 2)) + "%")
-    # print("Average testing set accuracy: " + str(round(letter_test_accuracies / N, 2)) + "%")
-    # print()
