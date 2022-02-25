754828abfc6ebf0a5f941657376bb84f1fd39982
labreezy <yourtooslow@gmail.com>
2022-02-25 11:39:11 -0600

    Basic case with 150 spiral points + folds, needs a few other things

--- preprocess.py
+++ preprocess.py
@@ -3,32 +3,121 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+
 def adults():
     features = {
         "age": (0, 100),
-        "workclass": ["Private", "Self-emp-not-inc", "Self-emp-inc", "Federal-gov", "Local-gov", "State-gov",
-                        "Without-pay", "Never-worked"],
+        "workclass": [
+            "Private",
+            "Self-emp-not-inc",
+            "Self-emp-inc",
+            "Federal-gov",
+            "Local-gov",
+            "State-gov",
+            "Without-pay",
+            "Never-worked",
+        ],
         "fnlwgt": (0, 1000000),
-        "education": ["Bachelors", "Some-college", "11th", "HS-grad", "Prof-school", "Assoc-acdm", "Assoc-voc", "9th",
-                        "7th-8th", "12th", "Masters", "1st-4th", "10th", "Doctorate", "5th-6th", "Preschool"],
+        "education": [
+            "Bachelors",
+            "Some-college",
+            "11th",
+            "HS-grad",
+            "Prof-school",
+            "Assoc-acdm",
+            "Assoc-voc",
+            "9th",
+            "7th-8th",
+            "12th",
+            "Masters",
+            "1st-4th",
+            "10th",
+            "Doctorate",
+            "5th-6th",
+            "Preschool",
+        ],
         "education-num": (0, 50),
-        "marital-status": ["Married-civ-spouse", "Divorced", "Never-married", "Separated", "Widowed",
-                            "Married-spouse-absent", "Married-AF-spouse"],
-        "occupation": ["Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial", "Prof-specialty",
-                        "Handlers-cleaners", "Machine-op-inspct", "Adm-clerical", "Farming-fishing", "Transport-moving",
-                        "Priv-house-serv", "Protective-serv", "Armed-Forces"],
-        "relationship": ["Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried"],
+        "marital-status": [
+            "Married-civ-spouse",
+            "Divorced",
+            "Never-married",
+            "Separated",
+            "Widowed",
+            "Married-spouse-absent",
+            "Married-AF-spouse",
+        ],
+        "occupation": [
+            "Tech-support",
+            "Craft-repair",
+            "Other-service",
+            "Sales",
+            "Exec-managerial",
+            "Prof-specialty",
+            "Handlers-cleaners",
+            "Machine-op-inspct",
+            "Adm-clerical",
+            "Farming-fishing",
+            "Transport-moving",
+            "Priv-house-serv",
+            "Protective-serv",
+            "Armed-Forces",
+        ],
+        "relationship": [
+            "Wife",
+            "Own-child",
+            "Husband",
+            "Not-in-family",
+            "Other-relative",
+            "Unmarried",
+        ],
         "race": ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"],
         "sex": ["Female", "Male"],
         "capital-gain": (0, 50000),
         "capital-loss": (0, 50000),
         "hours-per-week": (0, 24 * 7),
-        "native-country": ["United-States", "Cambodia", "England", "Puerto-Rico", "Canada", "Germany",
-                            "Outlying-US(Guam-USVI-etc)", "India", "Japan", "Greece", "South", "China", "Cuba", "Iran",
-                            "Honduras", "Philippines", "Italy", "Poland", "Jamaica", "Vietnam", "Mexico", "Portugal",
-                            "Ireland", "France", "Dominican-Republic", "Laos", "Ecuador", "Taiwan", "Haiti", "Columbia",
-                            "Hungary", "Guatemala", "Nicaragua", "Scotland", "Thailand", "Yugoslavia", "El-Salvador",
-                            "Trinadad&Tobago", "Peru", "Hong", "Holand-Netherlands"]
+        "native-country": [
+            "United-States",
+            "Cambodia",
+            "England",
+            "Puerto-Rico",
+            "Canada",
+            "Germany",
+            "Outlying-US(Guam-USVI-etc)",
+            "India",
+            "Japan",
+            "Greece",
+            "South",
+            "China",
+            "Cuba",
+            "Iran",
+            "Honduras",
+            "Philippines",
+            "Italy",
+            "Poland",
+            "Jamaica",
+            "Vietnam",
+            "Mexico",
+            "Portugal",
+            "Ireland",
+            "France",
+            "Dominican-Republic",
+            "Laos",
+            "Ecuador",
+            "Taiwan",
+            "Haiti",
+            "Columbia",
+            "Hungary",
+            "Guatemala",
+            "Nicaragua",
+            "Scotland",
+            "Thailand",
+            "Yugoslavia",
+            "El-Salvador",
+            "Trinadad&Tobago",
+            "Peru",
+            "Hong",
+            "Holand-Netherlands",
+        ],
     }
     examples = []
     with open("datasets/adult.data", "r") as train_file:
@@ -52,10 +141,11 @@ def adults():
                     "capital-loss": int(values[11]),
                     "hours-per-week": int(values[12]),
                     "native-country": values[13],
-                    "class": values[14]
+                    "class": values[14],
                 }
             )
     return features, examples[:2500]
+
 
 def blobs(plot=False):
     # dataset.blobs(200,[np.array([1, 1]), np.array([3, 1]), np.array([2, 4])],[np.array([[0.5, 0], [0, 0.5]]), np.array([[0.25, 0], [0, 0.25]]), np.array([[1, 0], [0, 1]])])
@@ -67,111 +157,133 @@ def blobs(plot=False):
         plt.show()
     return features, examples
 
+
 def digits():
     data = pd.read_csv("datasets/digits.csv").rename(columns={"label": "class"})
     examples = data.to_dict(orient="records")
     features = {c: (0, 255) for c in data.columns if c.startswith("pixel")}
     return features, examples[:250]
 
+
 def letters():
     features = {
-        "x-box horizontal position of box":     (0, 15),
-        "y-box vertical position of box":       (0, 15),
-        "width width of box":                   (0, 15),
-        "high height of box":                   (0, 15),
-        "onpix total # on pixels":              (0, 15),
-        "x-bar mean x of on pixels in box":     (0, 15),
-        "y-bar mean y of on pixels in box":     (0, 15),
-        "x2bar mean x variance":                (0, 15),
-        "y2bar mean y variance":                (0, 15),
-        "xybar mean x y correlation":           (0, 15),
-        "x2ybr mean of x * x * y":              (0, 15),
-        "xy2br mean of x * y * y":              (0, 15),
-        "x-ege mean edge count left to right":  (0, 15),
-        "xegvy correlation of x-ege with y":    (0, 15),
-        "y-ege mean edge count bottom to top":  (0, 15),
-        "yegvx correlation of y-ege with x":    (0, 15)
+        "x-box horizontal position of box": (0, 15),
+        "y-box vertical position of box": (0, 15),
+        "width width of box": (0, 15),
+        "high height of box": (0, 15),
+        "onpix total # on pixels": (0, 15),
+        "x-bar mean x of on pixels in box": (0, 15),
+        "y-bar mean y of on pixels in box": (0, 15),
+        "x2bar mean x variance": (0, 15),
+        "y2bar mean y variance": (0, 15),
+        "xybar mean x y correlation": (0, 15),
+        "x2ybr mean of x * x * y": (0, 15),
+        "xy2br mean of x * y * y": (0, 15),
+        "x-ege mean edge count left to right": (0, 15),
+        "xegvy correlation of x-ege with y": (0, 15),
+        "y-ege mean edge count bottom to top": (0, 15),
+        "yegvx correlation of y-ege with x": (0, 15),
     }
     examples = []
     train_file = open("datasets/letter-recognition.data", "r")
     lines = train_file.readlines()
     for line in lines:
         line = line[:-1]
-        values = line.replace("\"", "").replace(",", " ").split(" ")
-        examples.append({
-            "x-box horizontal position of box":     int(values[1]),
-            "y-box vertical position of box":       int(values[2]),
-            "width width of box":                   int(values[3]),
-            "high height of box":                   int(values[4]),
-            "onpix total # on pixels":              int(values[5]),
-            "x-bar mean x of on pixels in box":     int(values[6]),
-            "y-bar mean y of on pixels in box":     int(values[7]),
-            "x2bar mean x variance":                int(values[8]),
-            "y2bar mean y variance":                int(values[9]),
-            "xybar mean x y correlation":           int(values[10]),
-            "x2ybr mean of x * x * y":              int(values[11]),
-            "xy2br mean of x * y * y":              int(values[12]),
-            "x-ege mean edge count left to right":  int(values[13]),
-            "xegvy correlation of x-ege with y":    int(values[14]),
-            "y-ege mean edge count bottom to top":  int(values[15]),
-            "yegvx correlation of y-ege with x":    int(values[16]),
-            "class":                                str(values[0])
-        })
+        values = line.replace('"', "").replace(",", " ").split(" ")
+        examples.append(
+            {
+                "x-box horizontal position of box": int(values[1]),
+                "y-box vertical position of box": int(values[2]),
+                "width width of box": int(values[3]),
+                "high height of box": int(values[4]),
+                "onpix total # on pixels": int(values[5]),
+                "x-bar mean x of on pixels in box": int(values[6]),
+                "y-bar mean y of on pixels in box": int(values[7]),
+                "x2bar mean x variance": int(values[8]),
+                "y2bar mean y variance": int(values[9]),
+                "xybar mean x y correlation": int(values[10]),
+                "x2ybr mean of x * x * y": int(values[11]),
+                "xy2br mean of x * y * y": int(values[12]),
+                "x-ege mean edge count left to right": int(values[13]),
+                "xegvy correlation of x-ege with y": int(values[14]),
+                "y-ege mean edge count bottom to top": int(values[15]),
+                "yegvx correlation of y-ege with x": int(values[16]),
+                "class": str(values[0]),
+            }
+        )
     return features, examples[:500]
 
-def spirals(plot=False):
-    # dataset.spirals(1000, 2, 0.075)
+
+def spirals(plot=False, svm=False):
+    # dataset.spirals(100, 2, 0.075)
     data = pd.read_csv("datasets/spirals.csv", index_col=0)
     features = {"x": (0, 1), "y": (0, 1)}
+    if svm:
+
+        examples = data[["x", "y"]].to_numpy()
+        labels = data["class"].to_numpy()
+        labels[labels == 0] = -1
+        return labels, examples
     examples = data.to_dict(orient="records")
     if plot:
         plt.scatter(data["x"], data["y"], s=5, c=data["class"])
         plt.show()
     return features, examples
 
+
 def zoo():
     features = {
-        "hair":     [True, False],
+        "hair": [True, False],
         "feathers": [True, False],
-        "eggs":     [True, False],
-        "milk":     [True, False],
+        "eggs": [True, False],
+        "milk": [True, False],
         "airborne": [True, False],
-        "aquatic":  [True, False],
+        "aquatic": [True, False],
         "predator": [True, False],
-        "toothed":  [True, False],
+        "toothed": [True, False],
         "backbone": [True, False],
         "breathes": [True, False],
         "venemous": [True, False],
-        "fins":     [True, False],
-        "legs":     [0, 2, 4, 5, 6, 8],
-        "tail":     [True, False],
+        "fins": [True, False],
+        "legs": [0, 2, 4, 5, 6, 8],
+        "tail": [True, False],
         "domestic": [True, False],
-        "catsize":  [True, False]
+        "catsize": [True, False],
     }
     examples = []
     train_file = open("datasets/zoo.data", "r")
     lines = train_file.readlines()
-    classes = ["mammal", "bird", "reptile", "fish", "amphibian", "insect", "invertebrate"]
+    classes = [
+        "mammal",
+        "bird",
+        "reptile",
+        "fish",
+        "amphibian",
+        "insect",
+        "invertebrate",
+    ]
     for line in lines:
         line = line[:-1]
-        values = line.replace("\"", "").replace(",", " ").split(" ")
-        examples.append({
-            "hair":     bool(int(values[1])),
-            "feathers": bool(int(values[2])),
-            "eggs":     bool(int(values[3])),
-            "milk":     bool(int(values[4])),
-            "airborne": bool(int(values[5])),
-            "aquatic":  bool(int(values[6])),
-            "predator": bool(int(values[7])),
-            "toothed":  bool(int(values[8])),
-            "backbone": bool(int(values[9])),
-            "breathes": bool(int(values[10])),
-            "venemous": bool(int(values[11])),
-            "fins":     bool(int(values[12])),
-            "legs":     int(values[13]),
-            "tail":     bool(int(values[14])),
-            "domestic": bool(int(values[15])),
-            "catsize":  bool(int(values[16])),
-            "class":    classes[int(values[17]) - 1]
-        })
+        values = line.replace('"', "").replace(",", " ").split(" ")
+        examples.append(
+            {
+                "hair": bool(int(values[1])),
+                "feathers": bool(int(values[2])),
+                "eggs": bool(int(values[3])),
+                "milk": bool(int(values[4])),
+                "airborne": bool(int(values[5])),
+                "aquatic": bool(int(values[6])),
+                "predator": bool(int(values[7])),
+                "toothed": bool(int(values[8])),
+                "backbone": bool(int(values[9])),
+                "breathes": bool(int(values[10])),
+                "venemous": bool(int(values[11])),
+                "fins": bool(int(values[12])),
+                "legs": int(values[13]),
+                "tail": bool(int(values[14])),
+                "domestic": bool(int(values[15])),
+                "catsize": bool(int(values[16])),
+                "class": classes[int(values[17]) - 1],
+            }
+        )
     return features, examples
--- svm.py
+++ svm.py
@@ -1,40 +1,48 @@
 import numpy as np
 import numpy.random as nr
 
+import preprocess
 
-def apply_k_all(x, idx, kernel=np.inner):
-    res = np.apply_along_axis(lambda xi: kernel(xi, x[idx]), 1, x)
+
+def apply_k_all(x, inp, kernel=np.inner):
+    res = np.apply_along_axis(lambda xi: kernel(xi, inp), 1, x)
     return res
 
-def evaluate(example, alphas, b, kernel=np.inner):
-    kernel_res = np.apply_along_axis(lambda xi: kernel(xi, example), 1, )
 
-epsilon = .01
-def smo_train(examples, labels, c, tol, max_iter, kernel=np.inner):
+def evaluate(inp, x, y, a, b, kernel=np.inner):
+    kernel_res = apply_k_all(x, inp, kernel=kernel)
+    f_res = np.sum(a * y * kernel_res)
+    return np.sign(f_res + b)
+
+
+epsilon = 0.01
+
+
+def smo_train(x, y, c, tol, max_iter, kernel=np.inner):
     b = 0
-    m = len(labels)
+    m = len(y)
     alpha = np.zeros(m)
     passes = 0
     while passes < max_iter:
         n_changed_alpha = 0
         for i in range(m):
-            fx = apply_k_all(examples, i, kernel)
+            fx = apply_k_all(x, x[i], kernel)
 
-            fx = np.sum(alpha * labels * fx)
+            fx = np.sum(alpha * y * fx)
             fx += b
-            err_i = fx - labels[i]
-            if (labels[i] * err_i < -tol and alpha[i] < c) or (
-                labels[i] * err_i > tol and alpha[i] > 0
+            err_i = fx - y[i]
+            if (y[i] * err_i < -tol and alpha[i] < c) or (
+                y[i] * err_i > tol and alpha[i] > 0
             ):
                 j = i
                 while j == i:
                     j = nr.randint(0, m)
-                fx_j = apply_k_all(examples, j, kernel)
-                fx_j = np.sum(alpha * labels * fx_j) + b
-                err_j = fx_j - labels[j]
+                fx_j = apply_k_all(x, x[j], kernel)
+                fx_j = np.sum(alpha * y * fx_j) + b
+                err_j = fx_j - y[j]
                 ai_old = alpha[i]
                 aj_old = alpha[j]
-                if labels[i] != labels[j]:
+                if y[i] != y[j]:
                     L = max(0, alpha[j] - alpha[i])
                     H = min(c, c + alpha[j] - alpha[i])
                 else:
@@ -42,23 +50,21 @@ def smo_train(examples, labels, c, tol, max_iter, kernel=np.inner):
                     H = min(c, alpha[i] + alpha[j])
                 if L == H:
                     continue
-                eta = 2 * kernel(examples[i], examples[j]) - kernel(
-                    examples[i], examples[i]
-                ) - kernel(examples[j], examples[j])
+                eta = 2 * kernel(x[i], x[j]) - kernel(x[i], x[i]) - kernel(x[j], x[j])
                 if eta >= 0:
                     continue
-                alpha[j] = alpha[j] - (labels[j]*(err_i - err_j)) / eta
+                alpha[j] = alpha[j] - (y[j] * (err_i - err_j)) / eta
                 if alpha[j] < L:
                     alpha[j] = L
                 elif alpha[j] > H:
                     alpha[j] = H
                 if np.abs(alpha[j] - aj_old) < 0.001:
                     continue
-
-                alpha[i] += labels[i] * labels[j] * (aj_old - alpha[j])
-                b_part = labels[i] * (alpha[i] - ai_old) * kernel(
-                    examples[i], examples[i]
-                ) - labels[j] * (alpha[j] - aj_old) * kernel(examples[j], examples[j])
+                s = y[i] * y[j]
+                alpha[i] += s * (aj_old - alpha[j])
+                b_part = y[i] * (alpha[i] - ai_old) * kernel(x[i], x[i]) - y[j] * (
+                    alpha[j] - aj_old
+                ) * kernel(x[j], x[j])
                 b1 = b - err_i - b_part
                 b2 = b - err_j - b_part
                 if alpha[i] > 0 and alpha[i] < c:
@@ -70,15 +76,56 @@ def smo_train(examples, labels, c, tol, max_iter, kernel=np.inner):
                 n_changed_alpha += 1
         if n_changed_alpha == 0:
             passes = passes + 1
+            print(f"Passes: {passes}")
         else:
+            print("Reset Passes")
             passes = 0
     return alpha, b
 
+
+def n_folds_np(N, labels, examples):
+    indexes = np.arange(labels.shape[0])
+    nr.shuffle(indexes)
+    fold_indexes = np.asarray(np.split(indexes, N))
+    fold_list = []
+    for i in range(N):
+        test_idxs = fold_indexes[i]
+        train_idxs = np.concatenate(
+            fold_indexes[
+                np.arange(N) != i,
+            ],
+            axis=0,
+        )
+        test_labels = labels[test_idxs]
+        test_examples = examples[test_idxs]
+        train_labels = labels[train_idxs]
+        train_examples = examples[train_idxs]
+        fold = {
+            "train": (train_examples, train_labels),
+            "test": (test_examples, test_labels),
+        }
+        fold_list.append(fold)
+    return fold_list
+
+
 if __name__ == "__main__":
     nr.seed(1234)
-    examples = np.asarray([1,2,2,3,3,4,1,4,2,5,3,6]).reshape((6,2))
-    print(examples)
-    labels = np.asarray([-1,-1,-1,1,1,1])
-    a, b = smo_train(examples,labels,c=10,tol=0.001,max_iter=5  00)
-    print(a)
-    print(b)
+    labels, examples = preprocess.spirals(plot=True, svm=True)
+    # god this thing's slow so 150 it is
+    # TODO: cache dot products so recomputation is minimized
+    # TODO: implement a heuristic besides random selection to match Platt paper
+    labels = labels[:150]
+    examples = examples[:150]
+    folds = n_folds_np(5, labels, examples)
+    print("boop")
+    curr_fold_x, curr_fold_y = folds[0]["train"]
+    a, b = smo_train(curr_fold_x, curr_fold_y, c=100, tol=0.05, max_iter=3)
+    correct = 0
+    total = len(folds[0]["test"][1])
+    for idx in range(len(folds[0]["test"][1])):
+        test_ex = folds[0]["test"][0][idx]
+        test_lbl = folds[0]["test"][1][idx]
+        predicted = evaluate(test_ex, folds[0]["train"][0], folds[0]["train"][1], a, b)
+        if predicted == test_lbl:
+            correct += 1
+    print(f"Accuracy: {correct/total}")
